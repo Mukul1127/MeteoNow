3d045797240c08ad55fa62b98fdef253ed6a1956
Mukul Kedia <mukulkedia11@gmail.com>
2024-02-18 20:02:15 -0500

    Bye Bye! Import Aliases

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import path from "path"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
-  vite: {
-    resolve: {
-      alias: {
-        "@": path.resolve("./src/")
-      }
-    }
-  }
+  integrations: [tailwind(), compress()]
 });
