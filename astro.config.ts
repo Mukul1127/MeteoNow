340ee05de9a6a61d0223000cba81d5abe7f79c15
Mukul Kedia <mukulkedia11@gmail.com>
2024-02-18 19:50:06 -0500

    I'm stupid

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import path from "path"
 
 // https://astro.build/config
 export default defineConfig({
