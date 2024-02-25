620cd28f7916cdd4c1f49c7304fe11217a10b1ea
Mukul Kedia <mukulkedia11@gmail.com>
2024-02-25 10:41:54 -0500

    The Big License (Also I broke Current.astro)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MeteoWeather: rewrite
+# MeteoWeather v2 Rewrite
+
 Now In Astro!
+
+Check main branch for legacy code
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,21 @@
+/*
+  MeteoWeather is a Accurate Weather App 
+  Copyright (C) 2024 Mukul Kedia
+
+  MeteoWeather is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as published
+  by the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  MeteoWeather is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with MeteoWeather. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import type { Config } from "tailwindcss"
 import defaultTheme from "tailwindcss/defaultTheme"
 
@@ -14,7 +32,7 @@ export default {
       fontFamily: {
         sans: [
           "Inter", {
-            fontFeatureSettings: '"ss01", "cv05", "ss03"'
+            fontFeatureSettings: '"ss01", "cv05", "ss03", "case", "cv06", "cv11"'
           }, ...defaultTheme.fontFamily.sans
         ]
       },
